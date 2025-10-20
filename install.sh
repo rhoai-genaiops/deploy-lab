fa8806cdc4af87ed86afec8f1a8c0e22ac4a3f9c
Cansu Kavili <cansukavili@gmail.com>
2025-10-21 00:07:30 +0200

    🌼some updates during dry run

--- install.sh
+++ install.sh
@@ -14,24 +14,24 @@ helm upgrade --install ai501-student-content student-content --namespace ai501 -
 # Patch OAuth to point to ai501 htpasswd
 oc patch --type=merge OAuth/cluster -p '{"spec": {"identityProviders": [{"name": "Students", "type": "HTPasswd", "mappingMethod": "claim", "htpasswd": {"fileData": {"name": "htpasswd-ai501"}}}]}}'
 
-oc patch --type=merge DataScienceCluster/default-dsc -p '{"spec": {"components": {"trustyai": {"managementState": "Managed", "devFlags": {"manifests": [{"contextDir": "config", "sourcePath": "", "uri": "https://api.github.com/repos/ckavili/trustyai-service-operator-ci/tarball/service-acca8f52f3f163444b2fc68003af5cae13f04762"}]}}}}}'
+# oc patch --type=merge DataScienceCluster/default-dsc -p '{"spec": {"components": {"trustyai": {"managementState": "Managed", "devFlags": {"manifests": [{"contextDir": "config", "sourcePath": "", "uri": "https://api.github.com/repos/ckavili/trustyai-service-operator-ci/tarball/service-acca8f52f3f163444b2fc68003af5cae13f04762"}]}}}}}'
 
 oc -n openshift-user-workload-monitoring patch configmap user-workload-monitoring-config --type=merge -p '{"data": {"config.yaml": "prometheus:\n  logLevel: debug\n  retention: 15d\nalertmanager:\n  enabled: true\n  enableAlertmanagerConfig: true\n"}}'
 
 oc patch config.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge
 
 # Make ArgoCD cluster wide
-attendees=`grep attendees student-content/values.yaml | cut -d':' -f2`
- for ((i=0; i<=$attendees; i++))
- do
-   if [ $i -eq 1 ]; then
-     NS="user$i-toolings"
-   else
-     NS+="$var,user$i-toolings"
-   fi
- done
- oc -n openshift-gitops-operator patch subscriptions.operators.coreos.com/openshift-gitops-operator --type=json \
-         -p '[{"op":"'add'","path":"/spec/config/env", "value":[{"name": "DISABLE_DEFAULT_ARGOCD_INSTANCE", "value":"true"}] },{"op":"'add'","path":"/spec/config/env/1","value":{"name": "ARGOCD_CLUSTER_CONFIG_NAMESPACES", "value":"'${NS}'"}}]'
+# attendees=`grep attendees student-content/values.yaml | cut -d':' -f2`
+#  for ((i=0; i<=$attendees; i++))
+#  do
+#    if [ $i -eq 1 ]; then
+#      NS="user$i-toolings"
+#    else
+#      NS+="$var,user$i-toolings"
+#    fi
+#  done
+#  oc -n openshift-gitops-operator patch subscriptions.operators.coreos.com/openshift-gitops-operator --type=json \
+#          -p '[{"op":"'add'","path":"/spec/config/env", "value":[{"name": "DISABLE_DEFAULT_ARGOCD_INSTANCE", "value":"true"}] },{"op":"'add'","path":"/spec/config/env/1","value":{"name": "ARGOCD_CLUSTER_CONFIG_NAMESPACES", "value":"'${NS}'"}}]'
 
 
 oc patch tektonconfig config --type merge -p '{"spec":{"pipeline":{"disable-affinity-assistant":true}}}'
